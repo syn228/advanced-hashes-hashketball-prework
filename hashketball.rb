a3301034a5902e8f6c9c9e5255a2b0fd31309706
Aaron Na <syn228@nyu.edu>
2018-04-18 21:28:55 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,7 +201,7 @@ def big_shoe_rebounds
             if stats == :shoe
               if biggest_shoe == nil
                 biggest_shoe = stat_info
-                biggest_shoe_player_rebounds = game_hash
+                biggest_shoe_player_rebounds = game_hash[:location]
         end
       end
     end
