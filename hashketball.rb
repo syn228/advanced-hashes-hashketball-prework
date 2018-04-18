0de0ee11b046e77169bd3d18fb40507b47da0b25
Aaron Na <syn228@nyu.edu>
2018-04-18 20:38:11 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -161,9 +161,9 @@ def team_colors(team)
 end
 
 def team_names
-  teams = []
+  teams = ""
   game_hash.each do |location, team_data|
-    teams = team_data[:team_name].split(",")
+    teams << team_data[:team_name].split(",")
   end
   teams
 end
