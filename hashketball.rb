66b6e2193ffe2251a06dad583014611f89e015c5
Aaron Na <syn228@nyu.edu>
2018-04-18 20:57:34 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,12 +172,12 @@ def player_numbers(team)
   jerseys = []
   game_hash.each do |location, team_data|
       if team_data[:team_name] = team
-        team_data.each do |player
-        
+        team_data.each do |player, stats|
+        binding.pry
       end
     end
   end
-  jerseys
+  
 end
 
 
