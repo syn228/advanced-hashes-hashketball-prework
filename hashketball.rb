2b5cf5edc14e56940777d9e1bb4ee706749e79f9
Aaron Na <syn228@nyu.edu>
2018-04-18 21:24:33 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,6 +194,7 @@ def big_shoe_rebounds
   biggest_shoe = nil
   biggest_shoe_player_rebounds = nil
   game_hash.each do |location, team_data|
-    team.data.each do |
+    team.data.each do |idk, something|
+      binding.pry
   
 end
