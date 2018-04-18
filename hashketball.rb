90dc207a054868a53b2b2198b6829377a2e7635d
Aaron Na <syn228@nyu.edu>
2018-04-18 19:47:06 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,6 +163,7 @@ end
 def team_names
   game_hash.each do |location, team_data|
     team_data[:team_name].split.each do |team_name, name_of_team|
+      binding.pry
       return name_of_team
     end
   end
